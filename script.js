86b1aada877739dd0339ddaf01272312d3099712
djyamunaq <62448579+djyamunaq@users.noreply.github.com>
2021-10-22 09:42:17 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let apiQuotes = [];
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes)
+    } catch(error) {
+    }
+}
+
+getQuotes();
